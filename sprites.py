c36327ca0f1ca925d9e081ba618a55cc2ef827cb
cstout25 <161093102+cstout25@users.noreply.github.com>
2024-03-22 12:38:38 -0700

    Added faster movement after obtaining powerup

--- sprites.py
+++ sprites.py
@@ -32,13 +32,13 @@ class Player(pg.sprite.Sprite):
         self.vx, self.vy = 0, 0
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
-            self.vx = -PLAYER_SPEED
+            self.vx = -self.speed
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
-            self.vx = PLAYER_SPEED
+            self.vx = self.speed
         if keys[pg.K_UP] or keys[pg.K_w]:
-            self.vy = -PLAYER_SPEED
+            self.vy = -self.speed
         if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.vy = PLAYER_SPEED
+            self.vy = self.speed
         if self.vx != 0 and self.vy !=0:
                 self.vx * 0.7071
                 self.vy * 0.7071
